03a05d6f69aef811c353060f2b2af940f86934c0
Reitumetse Mafojane <mafojanemafole@gmail.com>
2026-05-31 17:19:42 +0200

    String manipulation

--- Chapters/Sequence-String and List/string.py
+++ Chapters/Sequence-String and List/string.py
@@ -13,6 +13,8 @@
 
 str = "This is a string"
 print (str)
+print (" ")
+
 str1 = "Python"
 print (str1[0])
 print (str1[1])
@@ -20,3 +22,25 @@ print (str1[2])
 print (str1[3])
 print (str1[4])
 print (str1[5])
+print (" ")
+#similar to str.length in Java
+for i in "Krishima":
+    print (i)
+print (" ")
+
+#Other concepts about string include the len() function for getting the length of a
+#string, keyword in, and not in for checking whether the substring or character is
+#present in a string.
+print (len(str))
+
+str2 = "Python is so simple"
+print ("simple" in str2)
+
+str3 = "Python is so simple"
+if "simple" in str3:
+    print ("Yes, 'simple' is present.")
+else :
+    print ("No, 'simple' is not present.")
+
+str4 = "Python is so simple"
+print ("programming" not in str4)
