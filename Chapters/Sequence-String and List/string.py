de501bcfdd30eccd4ee1fa396cb1381991dfaf3d
Reitumetse Mafojane <mafojanemafole@gmail.com>
2026-05-31 17:08:21 +0200

    New learning module

---
+++ Chapters/Sequence-String and List/string.py
@@ -0,0 +1,22 @@
+#A series of characters is referred to as a string. Python relies heavily on this idea
+#to function correctly. There are a few key aspects to consider about string.
+#Krishna Kumar Mohbey & Malika Acharya
+#All rights reserved-© 2023 Bentham Science Publishers
+#78   
+#Basics of Python Programming: A Quick Guide for Beginners
+#Mohbey and Acharya
+#Strings are amongst the most popular types in Python.
+#It can create them simply by enclosing characters in quotes (single, double, or
+#triple).
+#Python treats single quotes the same as double quotes.
+#A string is a sequence of Unicode characters, and a character is simply a symbol.
+
+str = "This is a string"
+print (str)
+str1 = "Python"
+print (str1[0])
+print (str1[1])
+print (str1[2])
+print (str1[3])
+print (str1[4])
+print (str1[5])
